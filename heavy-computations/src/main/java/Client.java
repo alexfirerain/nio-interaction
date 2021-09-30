44df7d7e30328fa8b4f86c1ac72aed4bc1f9d6a3
Alexander Lukyanov <lux-nova@mail.ru>
2021-10-01 02:42:44 +0300

    run

--- heavy-computations/src/main/java/Client.java
+++ heavy-computations/src/main/java/Client.java
@@ -6,37 +6,40 @@ import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Client {
+    public static final String HOSTNAME = "127.0.0.1";
+    public static final int PORT = 4160;
+
 
     public static void main(String[] args) throws IOException {
-        // Определяем сокет сервера
         final SocketChannel socketChannel = SocketChannel.open();
-        InetSocketAddress socketAddress = new InetSocketAddress("127.0.0.1", 23334);
-// подключаемся к серверу
-        socketChannel.connect(socketAddress);
 
-        try (Scanner scanner = new Scanner(System.in)) {
-// Определяем буфер для получения данных
-            final ByteBuffer inputBuffer = ByteBuffer.allocate(2 << 10);
-            System.out.println(new String(inputBuffer.array(), 0, socketChannel.read(inputBuffer),
-                    StandardCharsets.UTF_8).trim());
-            String msg;
+        try (socketChannel; Scanner scanner = new Scanner( System.in )) {
+            socketChannel.connect( new InetSocketAddress( HOSTNAME, PORT ) );
+            final ByteBuffer inputBuffer = ByteBuffer.allocate( 2 << 10 );
+            System.out.println( new String( inputBuffer.array(), 0,
+                    socketChannel.read( inputBuffer ), StandardCharsets.UTF_8 ).trim() );
+            inputBuffer.clear();
+            String input;
+
             while (true) {
-                System.out.println("Enter message for server...");
-                msg = scanner.nextLine();
-                if ("end".equals(msg)) break;
-                socketChannel.write(
-                        ByteBuffer.wrap(
-                                msg.getBytes(StandardCharsets.UTF_8)));
-                Thread.sleep(2000);
-                int bytesCount = socketChannel.read(inputBuffer);
-                System.out.println(new String(inputBuffer.array(), 0, bytesCount,
-                        StandardCharsets.UTF_8).trim());
+                input = scanner.nextLine();
+                if ("end".equals( input )) break;
+
+                socketChannel.write( ByteBuffer.wrap( input.getBytes( StandardCharsets.UTF_8 ) ) );
+
+                Thread.sleep( 2000 );
+
+                int bytesCount = socketChannel.read( inputBuffer );
+
+                System.out.println( new String( inputBuffer.array(),
+                        0,
+                        bytesCount,
+                        StandardCharsets.UTF_8 ).trim() );
+
                 inputBuffer.clear();
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
-        } finally {
-            socketChannel.close();
         }
 
 
--- heavy-computations/src/main/java/Server.java
+++ heavy-computations/src/main/java/Server.java
@@ -4,38 +4,74 @@ import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 public class Server {
+    public static final int CIT_LIMIT = 15;
 
     public static void main(String[] args) throws IOException {
+
         // Занимаем порт, определяя серверный сокет
         final ServerSocketChannel serverChannel = ServerSocketChannel.open();
-        serverChannel.bind(new InetSocketAddress("localhost", 23334));
+        serverChannel.bind(new InetSocketAddress("localhost", 4160));
         while (true) {
-// Ждем подключения клиента и получаем потоки для дальнейшей работы
+            // Ждем подключения клиента и получаем потоки для дальнейшей работы
             try (SocketChannel socketChannel = serverChannel.accept()) {
-// Определяем буфер для получения данных
+                // Определяем буфер для получения данных
                 final ByteBuffer inputBuffer = ByteBuffer.allocate(2 << 10);
 
-                socketChannel.write(ByteBuffer.wrap((
-                        "Здравствуйте, вас приветствует " + Computer.NAME + "!"
-                ).getBytes(StandardCharsets.UTF_8)));
+                socketChannel.write(
+                        transmit("""
+                                Здравствуйте, вас приветствует %s!
+                                Сегодня вам доступно вычисление N-го члена Фибоначчи!
+                                Чтобы воспользоваться услугой, отправьте команду 'fib x', где 'x' – интересующий вас член."""
+                ));
+                System.out.println("Отправлено приветствие");
 
                 while (socketChannel.isConnected()) {
-// читаем данные из канала в буфер
                     int bytesCount = socketChannel.read(inputBuffer);
-// если из потока читать нельзя, перестаем работать с этим клиентом
                     if (bytesCount == -1) break;
 
-
-// получаем переданную от клиента строку в нужной кодировке и очищаем буфер
-                    final String msg = new String(inputBuffer.array(), 0, bytesCount,
-                            StandardCharsets.UTF_8);
+                    String clientsRequest = new String(inputBuffer.array(), 0, bytesCount, StandardCharsets.UTF_8);
                     inputBuffer.clear();
-                    System.out.println("Получено сообщение от клиента: " + msg);
-// отправляем сообщение клиента назад с пометкой ЭХО
-                    socketChannel.write(ByteBuffer.wrap(("Эхо: " +
-                            msg).getBytes(StandardCharsets.UTF_8)));
+                    System.out.println("Получено от клиента: " + clientsRequest);
+
+                    if (!clientsRequest.startsWith("fib")) {
+                        socketChannel.write(echo(clientsRequest,
+                                "это конечно хорошо, но как насчёт Фибоначчи?" )
+                        );
+                        continue;
+                    }
+
+                    String[] request = clientsRequest.split(" ");
+                    if (request.length < 2) {
+                        socketChannel.write(echo(clientsRequest,
+                                "это прикольно, но нужно соблюсти формат." )
+                        );
+                        continue;
+                    }
+
+                    int argument;
+                    try {
+                        argument = Integer.parseInt(request[1]);
+                    } catch (NumberFormatException e) {
+                        socketChannel.write(echo(clientsRequest,
+                                "это прикольно, но нужно соблюсти формат." )
+                        );
+                        continue;
+                    }
+
+                    socketChannel.write(transmit(computingResponse(argument)));
+                    socketChannel.write(transmit("Команда получена!"));
+//                    long result = Computer.fibonacci(argument);
+//                    System.out.println("Вычислен результат: " + result);
+//                    socketChannel.write(ByteBuffer.wrap(
+//                            "Вычислен %d-й член: %d"
+//                            .formatted(argument, result)
+//                            .getBytes(StandardCharsets.UTF_8))
+//                    );
+
+
                 }
 
             } catch (IOException err) {
@@ -43,5 +79,35 @@ public class Server {
             }
         }
     }
+
+    private static ByteBuffer echo(String msg, String answer) {
+        return ByteBuffer.wrap(
+                "%s – %s"
+                .formatted(
+                        msg.length() < CIT_LIMIT ?
+                                msg : msg.substring(0, CIT_LIMIT) + "...",
+                        answer)
+                .getBytes(StandardCharsets.UTF_8)
+        );
+    }
+
+    private static ByteBuffer transmit(String msg) {
+        return ByteBuffer.wrap(msg.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private static String computingResponse(int arg) {
+        long res;
+        long timeOn = new Date().getTime();
+        try {
+            res = Computer.fibonacci(arg);
+            Thread.sleep(1500);
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+        long time = new Date().getTime() - timeOn;
+        return "%d-й член Фибоначчи вычислен за %s: %d%n"
+                .formatted(arg, Computer.nanoTimeFormatter(time), res);
+    }
+
 
 }
